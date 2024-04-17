1141440557d285e64c18ddb65c21392f1eac6f72
EastHuangSineWave <1316098218@qq.com>
2024-04-17 17:12:00 +0800

    添加了一个windows下可以用的https的身份验证，但注意ssl是要先在win系统层绑定到端口的

--- src/Lair.HttpListenerConsole/Program.cs
+++ src/Lair.HttpListenerConsole/Program.cs
@@ -13,8 +13,10 @@ namespace Lair.HttpListenerConsole
         {
             var rawHttpHandler = new RawHttpHandler();
             AsyncHttpListener asyncHttpListener = new Lair.HttpListener.AsyncHttpListener(rawHttpHandler);
-            asyncHttpListener.Initialize(new string[] { "http://+:8082" }, 5000);
+            asyncHttpListener.Initialize(new string[] { "http://127.0.0.1:8888/", "https://127.0.0.1:6666/" }, 5000);
             asyncHttpListener.Start();
+
+            //netsh http add sslcert ipport=0.0.0.0:8083 certhash=CERT_THUMBPRINT appid={GUID}
 
             Console.WriteLine("Listening...");
             Console.ReadLine();
