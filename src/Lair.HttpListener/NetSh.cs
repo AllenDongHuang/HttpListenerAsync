93a916333d7dbcec1c6b8a6cef598f0c1b974bc0
EastHuangSineWave <1316098218@qq.com>
2024-04-17 17:39:04 +0800

    修改成一个适用于windows环境下验证ssl证书监听的httplistener，但是ssl证书还是需要win系统层命令行绑定
    主要是用了WindowsIdentity，也就是用了windows用户的验证，在系统信任的证书里添加了监听的url信任的ssl证书即可使用。

---
+++ src/Lair.HttpListener/NetSh.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lair.HttpListener
+{
+    public static class NetSh
+    {
+        private const string NetshCommand = "netsh";
+
+        /// <summary>
+        /// Add a url reservation
+        /// </summary>
+        /// <param name="url">Url to add</param>
+        /// <param name="user">User to add the reservation for</param>
+        /// <returns>True if successful, false otherwise.</returns>
+        public static bool AddUrlAcl(string url, string user)
+        {
+            try
+            {
+                var arguments = GetAddParameters(url, user);
+
+                return UacHelper.RunElevated(NetshCommand, arguments);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        
+        /// <summary>
+        /// Delete a url reservation
+        /// </summary>
+        /// <param name="url">Url to delte</param>
+        /// <param name="user">User to delete the reservation for</param>
+        /// <returns>True if successful, false otherwise.</returns>
+        public static bool DeleteUrlAcl(string url, string user)
+        {
+            try
+            {
+                var arguments = GetDeleteParameters(url, user);
+
+                return UacHelper.RunElevated(NetshCommand, arguments);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        internal static string GetAddParameters(string url, string user)
+        {
+            return string.Format("http add urlacl url=\"{0}\" user=\"{1}\"", url, user);
+        }
+
+        internal static string GetDeleteParameters(string url, string user)
+        {
+            return string.Format("http delete urlacl url=\"{0}\"", url);
+        }
+    }
+}
